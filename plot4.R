19cade5548511afadfae18335fd210a760545e1d
akrasnyy <akrasnyy@gmail.com>
2015-09-12 12:09:44 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -35,17 +35,12 @@ plot(sub_data$Datetime,sub_data$Sub_metering_1
      ) 
 lines(datetime, sub_data$Sub_metering_2, type="l", col="red") 
 lines(datetime, sub_data$Sub_metering_3, type="l", col="blue") 
-##legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
        , lty=1
        , seg.len=0.5 
        , bty = "n"
-
-##       , lwd=1
        , col=c("black", "red", "blue") 
        , y.intersp = 0.5
-##       , xjust= 1
-##       , yjust= 1
        )
 
 ## Plot 3
